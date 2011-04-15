4c2cea842db6d7a04719297a673aec21264e200c
Rafael Benevides <rafabene@gmail.com>
2011-04-15 17:55:39 -0300

    Modificado as libs

--- jbpmExample/src/hot/org/domain/jbpmexample/session/CompraService.java
+++ jbpmExample/src/hot/org/domain/jbpmexample/session/CompraService.java
@@ -1,11 +1,21 @@
 package org.domain.jbpmexample.session;
 
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Log;
 
 @Name("compraService")
 public class CompraService {
+    
+    @Logger
+    private Log log;
+    
+    @In
+    @SuppressWarnings("unused")
+    private AprovacaoCompraProcess aprovacaoCompraProcess;
 
 	public void registrarCompra(){
-		System.out.println("REGISTRADO");
+		log.info("Compra registrada para o item com valor #{aprovacaoCompraProcess.tamanhoCompra}");
 	}
 }
